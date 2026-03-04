d70d0cd492a75a72b17fcb3ae828fb61dc97056a
Anshul Prasad <anshulprasad5440.com>
2026-03-04 18:37:13 +0530

    use multiple chunks.

--- src/preprocess.py
+++ src/preprocess.py
@@ -12,29 +12,30 @@ import numpy as np
 class Preprocessor:
     def __init__(self, audio, original_sr):
         self.audio = audio
-        self.original_sr = original_sr
+        self.orig_sr = original_sr
 
     def resample_audio(self, target_sr=22050):
-        if self.original_sr == target_sr:
-            resampled = self.audio.astype(np.float32)
+        if self.orig_sr == target_sr:
+            resampled = self.audio
             return resampled
-        resampled = librosa.resample(self.audio, self.original_sr, self.target_sr)
-        return resampled.astype(np.float32)
+        resampled = librosa.resample(self.audio, orig_sr=self.orig_sr, target_sr=target_sr)
+        return resampled
 
     def to_mono(self, audio):
         if audio.ndim == 1:
-            return audio.astype(np.float32)
+            return audio
         monoed = librosa.to_mono(audio)
-        return monoed.astype(np.float32)
+        return monoed
 
     def trim_silence(self, audio):
         audio_trim, _ = librosa.effects.trim(audio)
-        return audio_trim.astype(np.float32)
+        return audio_trim
 
     def chunk_audio(self, audio, sr=22050, duration=5):
-        clip = audio[:sr * duration]  # take first 5 seconds
-        clip = librosa.util.fix_length(clip, size=sr * duration)  # pad if short
-        return clip.astype(np.float32)
+        chunk_size = sr * duration
+        chunks = [audio[i:i+chunk_size] for i in range(0, len(audio), chunk_size)]
+        chunks = [librosa.util.fix_length(c, size=chunk_size) for c in chunks]
+        return chunks # returns list of chunks
 
     def save_audio(self, audio, output_path, sr=22050):
         sf.write(output_path, audio, samplerate=sr)
