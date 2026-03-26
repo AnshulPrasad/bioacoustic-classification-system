7e2648ca8d10eb0bd6c20e3b2a32b69ede565fe4
Anshul Prasad <anshulprasad5440.com>
2026-03-26 15:00:06 +0530

    variable names grammar corrected Minor comments PROCESSED_DIR hardcoding removed

--- src/__init__.py
+++ src/__init__.py
@@ -42,21 +42,20 @@ def preprocess():
                     audio, sr = librosa.load(audio_path, sr=22050)
                     obj = Preprocessor(audio, sr)
                     resampled = obj.resample_audio(sr)
-                    monoed = obj.to_mono(resampled)
-                    trimed = obj.trim_silence(monoed)
-                    chunks = obj.chunk_audio(trimed, sr, 5)
-                    folder_path = Path(f"../data/processed/{'_'.join(species_folder.stem.split('_')[:-1])}_wav")
+                    mono = obj.to_mono(resampled)
+                    trimmed = obj.trim_silence(mono)
+                    chunks = obj.chunk_audio(trimmed, sr, 5)
+                    folder_path = Path(f"{PROCESSED_DIR}/{'_'.join(species_folder.stem.split('_')[:-1])}_wav")
                     folder_path.mkdir(parents=True, exist_ok=True)
-                    for i, chunk in enumerate(chunks):
+                    for i, chunk in enumerate(chunks): # save all the chunks of the audio file in the folder PROCESSED_DIR
                         file_path =  Path(f"{audio_path.stem}_chunk{i}").with_suffix(".wav")
                         output_path = folder_path / file_path
                         if output_path.exists():
                             logger.info("Already exist: %s", output_path)
                             continue
                         obj.save_audio(chunk, output_path, sr=22050)
-                        logger.info("Saved %s", output_path)
-                    del obj
-                    logger.info("Prerocessed %s", audio_path)
+                        logger.info("Saved chunk: %s", output_path)
+                    logger.info("Preprocessed: %s", audio_path)
                 except Exception as e:
                     logger.error("Skipping %s: %s", audio_path, e)
 
