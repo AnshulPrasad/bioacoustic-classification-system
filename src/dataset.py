bbe1964bb53bace7a0258e2964c78da4f6504366
Anshul Prasad <anshulprasad5440.com>
2026-03-26 19:26:27 +0530

    minor logging update

--- src/dataset.py
+++ src/dataset.py
@@ -16,6 +16,7 @@ class BirdSoundDataset(Dataset):
     def __init__(self, SPLIT_DIR, RAW_DIR, split='train', transform=None):
         self.SPLIT_DIR = Path(SPLIT_DIR) / split
         self.RAW_DIR = RAW_DIR
+        self.split = split
         self.files = sorted(self.SPLIT_DIR.rglob('*.png'))
         self. labels = self.labels_int()
         self.transform = transform or transforms.Compose([
@@ -25,17 +26,16 @@ class BirdSoundDataset(Dataset):
         ])
 
     def load_all_metadata(self):
-        logger.info('Loading all metadata')
+        logger.info(f'Loading all metadata({self.split})')
         df = pd.concat([pd.read_csv(f, usecols=['id', 'type']) for f in sorted(Path(self.RAW_DIR).glob("*.csv"))],ignore_index=True)
         df['id'] = df['id'].astype(str)
         le = LabelEncoder()
         df['label'] = le.fit_transform(df['type'])
         self.num_classes = df['label'].max() + 1
-        logger.info('Loaded all metadata')
         return df.set_index('id')['label']  # id → int label
 
     def labels_int(self):
-        logger.info('Loading labels')
+        logger.info(f'Loading labels({self.split})')
         ids = [file.stem.split('_')[-3] for file in self.files]  # extract all IDs at once
         logger.info('Loaded labels')
         return self.load_all_metadata().reindex(ids, fill_value=-1).tolist()
