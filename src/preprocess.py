67efcafe36c00886ef5fbcd66c43261917be7aff
Anshul Prasad <anshulprasad5440.com>
2026-03-28 21:29:24 +0530

    change class constructor input

--- src/preprocess.py
+++ src/preprocess.py
@@ -11,10 +11,8 @@ from logger import get_logger
 logger = get_logger(__name__, 'preprocess.log')
 
 class Preprocessor:
-    def __init__(self, audio, original_sr):
-        self.audio = audio
-        self.orig_sr = original_sr
     def __init__(self, audio_path: Path):
+        self.audio, self.sr = librosa.load(audio_path, sr=22050)
 
     def resample_audio(self, target_sr=22050):
         if self.orig_sr == target_sr:
@@ -24,25 +22,25 @@ class Preprocessor:
         resampled = librosa.resample(self.audio, orig_sr=self.orig_sr, target_sr=target_sr)
         return resampled
 
-    def to_mono(self, audio):
+    @staticmethod
     def to_mono(audio: np.ndarray):
         if audio.ndim == 1:
             return audio
         monoed = librosa.to_mono(audio)
         return monoed
 
-    def trim_silence(self, audio):
+    @staticmethod
     def trim_silence(audio: np.ndarray):
         audio_trim, _ = librosa.effects.trim(audio)
         return audio_trim
 
-    def chunk_audio(self, audio, sr=22050, duration=5):
+    @staticmethod
     def chunk_audio(audio: np.ndarray, sr: int =22050, duration: int = 5):
         chunk_size = sr * duration
         chunks = [audio[i:i+chunk_size] for i in range(0, len(audio), chunk_size)]
         chunks = [librosa.util.fix_length(c, size=chunk_size) for c in chunks]
         return chunks # returns list of chunks
 
-    def save_audio(self, audio, output_path, sr=22050):
+    @staticmethod
     def save_audio(audio: np.ndarray, output_path: Path, sr: int = 22050):
         sf.write(output_path, audio, samplerate=sr)
