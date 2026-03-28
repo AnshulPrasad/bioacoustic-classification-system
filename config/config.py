4c8851e4ca10d794fdaa0cecba33926b18975c24
Anshul Prasad <anshulprasad5440.com>
2026-03-28 19:30:39 +0530

    improve readability

--- config/config.py
+++ config/config.py
@@ -10,14 +10,14 @@ SPECIES_LIST = [
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
-RAW_DIR = PROJECT_ROOT / "data" / "raw"
-PROCESSED_DIR = PROJECT_ROOT / "data" / "processed"
-SPECTROGRAM_DIR = PROJECT_ROOT / "data" / "spectrograms"
-SPLIT_DIR = PROJECT_ROOT / 'data" / "split'
-MODEL_PATH = PROJECT_ROOT / "models" / "checkpoints" / "best_model.pth"
+RAW_DIR =               PROJECT_ROOT / "data"    / "raw"
+PROCESSED_DIR =         PROJECT_ROOT / "data"    / "processed"
+SPECTROGRAM_DIR =       PROJECT_ROOT / "data"    / "spectrograms"
+SPLIT_DIR =             PROJECT_ROOT / 'data"    / "split'
+MODEL_PATH =            PROJECT_ROOT / "models"  / "checkpoints"            / "best_model.pth"
 CONFUSION_MATRIX_PATH = PROJECT_ROOT / "outputs" / "confusion_matrix.png"
-SPLIT_JSON_PATH = PROJECT_ROOT / "models" / "split_index.json"
-CLASS_MAPPING_JSON = PROJECT_ROOT / "models" / "class_mapping.json"
+SPLIT_JSON_PATH =       PROJECT_ROOT / "models"  / "split_index.json"
+CLASS_MAPPING_JSON =    PROJECT_ROOT / "models"  / "class_mapping.json"
 
 for p in [RAW_DIR, PROCESSED_DIR, SPECTROGRAM_DIR, SPLIT_DIR, MODEL_PATH, CONFUSION_MATRIX_PATH, SPLIT_JSON_PATH, CLASS_MAPPING_JSON]:
     p.mkdir(parents=True, exist_ok=True)
