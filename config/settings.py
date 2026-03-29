a9259083f97e2d0bc7284604585b1c0e5b05ee6e
Anshul Prasad <anshulprasad5440.com>
2026-03-29 17:29:01 +0530

    fix: anshulprasad-avian-vocal-classification-system.hf.space refused to connect.

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ CSRF_TRUSTED_ORIGINS = ['https://*.hf.space', 'https://*.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
